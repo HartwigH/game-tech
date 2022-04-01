ee377b85d981d748e8431d2f3a910472456166ac
jaosaari <jaosaari@gmail.com>
2022-04-01 13:39:42 +0300

    chore(project structure): folder moves/adds

---
+++ src/constants/breakpoints.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-shadow
+enum breakpoints {
+  PHONE_MAX_WIDTH = 719,
+  TABLET_MIN_WIDTH = 720,
+  TABLET_MAX_WIDTH = 1439,
+  DESKTOP_MIN_WIDTH = 1440,
+  XSMALL = 0,
+  XSMALL_MAX = 599,
+  SMALL = 600,
+  SMALL_MAX = 1023,
+  MEDIUM = 1024,
+  MEDIUM_MAX = 1439,
+  LARGE = 1440,
+  LARGE_MAX = 1919,
+  XLARGE = 1920,
+}
+
+export default breakpoints;
